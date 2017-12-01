dc1ce29be84b90e8298c05d5409f58e0c46f82d7
DESKTOP-19689J4\Nihal <nihals25@gmail.com>
2017-12-01 00:22:29 -0600

    phone number validation added in the signup page

--- public/javascripts/ogstore.js
+++ public/javascripts/ogstore.js
@@ -279,4 +279,24 @@ app.service('commonservice',function()
         getIsLoggedIn : getIsLoggedIn,
         setIsLoggedIn : setIsLoggedIn
     }
+    });
+
+app.directive('numbersOnly', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attr, ngModelCtrl) {
+                function fromUser(text) {
+                    if (text) {
+                        var transformedInput = text.replace(/[^0-9-]/g, '');
+                        if (transformedInput !== text) {
+                            ngModelCtrl.$setViewValue(transformedInput);
+                            ngModelCtrl.$render();
+                        }
+                        return transformedInput;
+                    }
+                    return undefined;
+                }
+            ngModelCtrl.$parsers.push(fromUser);
+        }
+    };
 });
